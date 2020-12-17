6c2f2ddae9ef5deae09fafd1157786e723319eee
Hugo Carvalho <webnoico1978@gmail.com>
2020-12-17 22:51:33 +0100

    Added hover effect to word once game has ended

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -3,26 +3,28 @@ import "./Word.scss";
 
 function Word({ gameStatus, scrambledWord, correctWord }) {
   const link = `https://en.wikipedia.org/wiki/${scrambledWord}`;
+  const [makeColorPink, setmakeColorPink] = React.useState(false);
+
   return (
     <div className="Word">
       {/* <span>word</span> */}
       <span>
-        {
-          gameStatus === "onLoad" || gameStatus === "scramblingWord" ? (
-            "❓"
-          ) : gameStatus === "ended" ? (
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              {scrambledWord}{" "}
-            </a>
-          ) : (
-            scrambledWord
-          )
-          //gameStatus === "playing"
-          //? scrambledWord
-          //: gameStatus === "ended"
-          //? correctWord
-          //: null
-        }
+        {gameStatus === "onLoad" || gameStatus === "scramblingWord" ? (
+          "❓"
+        ) : gameStatus === "ended" ? (
+          <a
+            style={makeColorPink ? { color: "pink", transform: "scale(1.1)" } : null}
+            onMouseOver={() => setmakeColorPink(true)}
+            onMouseLeave={() => setmakeColorPink(false)}
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {scrambledWord}{" "}
+          </a>
+        ) : (
+          scrambledWord
+        )}
       </span>
     </div>
   );
