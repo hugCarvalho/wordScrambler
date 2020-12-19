b3c64fdd4d3bdf97d71a2e49d774b1cd4f795755
Hugo Carvalho <webnoico1978@gmail.com>
2020-12-19 23:48:50 +0100

    Fixed inconsistency between firefox and Chrome when displaying game-end victor animation

--- src/components/AnimationsDisplay/AnimationsDisplay.js
+++ src/components/AnimationsDisplay/AnimationsDisplay.js
@@ -3,25 +3,27 @@ import "./AnimationsDisplay.scss";
 
 const animations = ["🏆", "🥇", "🥈", "🥉", "🏅", "👏"];
 function AnimationsDisplay({ gameWon }) {
-  const [randomNum, setRandomNum] = React.useState(null);
+  //const [randomNum, setRandomNum] = React.useState(null);
+  const [randomNum, setRandomNum] = React.useState(2);
 
-  React.useEffect(() => {
-    if (gameWon) {
-      setRandomNum(Math.floor(Math.random() * 3));
-    }
-  }, [gameWon]);
+  // React.useEffect(() => {
+  //   if (gameWon) {
+  //     setRandomNum(Math.floor(Math.random() * 3));
+  //   }
+  // }, [gameWon]);
   console.log(randomNum);
   return (
     <section
       style={gameWon ? { backgroundColor: "white" } : null}
       className={`AnimationsDisplay ${gameWon && "activate-game-end"}  `}
     >
-      {/* {gameWon && randomToken()} */}
+      <div></div>
       <span className={`medal ${gameWon && randomNum === 1 && "activate-medal"}`}>
         🏅
       </span>
       <span className={`clap ${gameWon && randomNum === 2 && "activate-clap"}`}>👏</span>
       <span className={`trophy ${gameWon && randomNum === 0 && "activate-trophy"}`}>
+        {/* {gameWon && randomToken()} */}
         🏆
       </span>
     </section>
