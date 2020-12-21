f185911589c84a583fb6a1eec0493f7b68bc98c0
Hugo Carvalho <webnoico1978@gmail.com>
2020-12-21 23:59:19 +0100

    .

--- src/data/testData.js
+++ src/data/testData.js
@@ -1,21 +1,28 @@
 //"whale", "camel", "horse", "python"
-const testData = ["MiKE"];
-// const testData = [
-//   "car",
-//   "oven",
-//   "fork",
-//   "paper",
-//   "fridge",
-//   "piano",
-//   "pillow",
-//   "garlic",
-//   "wardrobe",
-//   "backpack",
-//   "suitcase",
-//   "painting",
-//   "trousers",
-//   "headphone",
-// ];
+//const testData = ["MiKE"];
+const testData = [
+  "car",
+  "oven",
+  "fork",
+  "paper",
+  "fridge",
+  "piano",
+  "pillow",
+  "garlic",
+  "wardrobe",
+  "backpack",
+  "suitcase",
+  "painting",
+  "trousers",
+  "headphone",
+  "computer",
+  "mobile",
+  "scissors",
+  "heating",
+  "chair",
+  "poster",
+  "boardgame",
+];
 //, "12", "23", "43", "53"];
 //const initArr = ["da"];
 
